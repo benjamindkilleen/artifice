7c28013770798bcf3fb0a8fd20a614365bba6392
killeen <killeen@uchicago.edu>
2018-12-15 12:42:54 -0600

    updated README

--- reports/fall_2018_report/auto/report.el
+++ reports/fall_2018_report/auto/report.el
@@ -5,6 +5,13 @@
                      '(("IEEEtran" "10pt" "journal")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("subfig" "caption=false" "font=footnotesize")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "IEEEtran"
@@ -15,6 +22,9 @@
     "hyperref"
     "graphicx"
     "subfig")
+   (TeX-add-symbols
+    "endthebibliography"
+    "vol")
    (LaTeX-add-labels
     "sec:introduction"
     "fig:traditional-graph"
@@ -29,8 +39,6 @@
     "sec:simulated-experiments"
     "sec:conclusion"
     "sec:acknowledgment")
-   (LaTeX-add-bibliographies
-    "IEEEabrv"
-    " report"))
+   (LaTeX-add-bibliographies))
  :latex)
 
