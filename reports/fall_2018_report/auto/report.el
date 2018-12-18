dfeae63e5e1213c2b00f92a873f6131b09dceb83
killeen <killeen@uchicago.edu>
2018-12-17 20:54:27 -0600

    updated fall report

--- reports/fall_2018_report/auto/report.el
+++ reports/fall_2018_report/auto/report.el
@@ -5,12 +5,12 @@
                      '(("IEEEtran" "10pt" "journal")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("subfig" "caption=false" "font=footnotesize")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -21,23 +21,23 @@
     "amssymb"
     "hyperref"
     "graphicx"
-    "subfig")
+    "subfig"
+    "siunitx")
    (TeX-add-symbols
     "endthebibliography"
-    "vol")
+    "vol"
+    "argmax")
    (LaTeX-add-labels
     "sec:introduction"
+    "fig:free-fall"
     "fig:traditional-graph"
     "fig:artifice-graph"
     "fig:dependency-graphs"
-    "sec:governing-principles"
-    "sec:related-work"
+    "sec:method"
     "fig:gyros"
     "fig:coupled-spheres"
     "fig:example-experiments"
-    "sec:method"
-    "sec:simulated-experiments"
-    "sec:conclusion"
+    "sec:discussion"
     "sec:acknowledgment")
    (LaTeX-add-bibliographies))
  :latex)
