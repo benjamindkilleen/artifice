Fixed scaling bug

artifice/utils/img.py
@@ -186,15 +186,22 @@ def as_float(image, atleast_3d=True):
   else:
     return image
 
-  
+def as_uint(image):
+  if image.dtype in [np.float32, np.float64]:
+    image = (255*image).astype(np.uint8)
+  elif image.dtype in [np.uint8, np.int32, np.int64]:
+    image = image.astype(np.uint8)
+  else:
+    raise ValueError(f"image dtype '{image.dtype}' not allowed")
+  return image
+
 def open_as_float(image_path):
   return as_float(open_as_array(image_path), atleast_3d=False)
 
 
 def save(fname, image):
   """Save the array image to png in fname."""
-  if image.dtype in [np.float32, np.float64]:
-    image = np.uint8(255*image)
+  image = as_uint(image)
   im = Image.fromarray(image)
   im.save(fname)
 

artifice/utils/vid.py
@@ -5,6 +5,7 @@ import subprocess as sp
 import logging
 import os
 import matplotlib.pyplot as plt
+from artifice.utils import img
 
 logger = logging.getLogger('artifice')
 
@@ -67,7 +68,7 @@ class MP4Writer:
   def write(self, frame):
     if self.shape is None:
       self.open(frame.shape)
-    frame = frame.astype(np.uint8)
+    frame = img.as_uint(frame)
     if frame.shape[2] == 3:
       frame = np.insert(frame, 3, np.zeros((frame.shape[:2])), axis=2)
     self.proc.stdin.write(frame.tobytes())