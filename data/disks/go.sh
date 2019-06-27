625e7d6348be200b2a146c03e4cd2fc9dfde5281
Gordon Kindlmann <glk@uchicago.edu>
2019-06-27 10:34:30 -0500

    gendata.sh -> genimgs.sh

--- data/disks/go.sh
+++ data/disks/go.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# get a unu GLK trusts
+# in rcc-land, get a unu GLK trusts
 export PATH=/home/glk/teem-install/bin:$PATH
 
 N=2000 # how many images to make
 
-mkdir images
-mkdir labels
+if [ ! -d images ]; then
+    mkdir images
+fi
+if [ ! -d labels ]; then
+    mkdir labels
+fi
 rm -f labels/????.txt images/????.png
 for I in $(seq $N); do
     echo $I/$N
     II=$(printf %04d $[$I+1000])
     ./genobjs.sh $II
-    ./gendata.sh $II
+    ./genimgs.sh $II
 done
 
